f798dbff2124b5469c3100acf2e94f2f1e40a857
MrBarker47 <jebarker7@gmail.com>
2024-12-12 08:04:04 -0500

    Update files

--- sba-320/src/components/PokeDex.jsx
+++ sba-320/src/components/PokeDex.jsx
@@ -1,1 +1,30 @@
-export default Pokemon 
+import {useState, useEffect} from "react"
+
+export default function Pokedex(props) {
+    const [PokedexData, setPokedex] = useState({
+      searchterm: ""  
+    });
+
+    const handleChange = (event) => {
+        setPokedex({ ...PokedexData, [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        props.pokemonsearch(PokedexData.searchterm);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                name="searchterm"
+                onChange={handleChange}
+                value={PokedexData.searchterm}
+                />
+            </form>
+        </div>
+    );
+}
