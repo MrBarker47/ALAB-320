2cf180ac582610bb60bede654919023a6ed2db51
MrBarker47 <jebarker7@gmail.com>
2024-12-13 10:24:11 -0500

    Update files

---
+++ sba-320/readMe2024.md
@@ -0,0 +1,3 @@
+# Read Me File
+
+This is a PokeDex, that gives you info on the pokemon that you typed in. 
--- sba-320/src/components/PokemonDisplay.jsx
+++ sba-320/src/components/PokemonDisplay.jsx
@@ -5,27 +5,35 @@ import {useState, useEffect} from 'react'
 
 export default function App() {
     //Pokemon API
-    const apiURL = "https://pokeapi.co/api/v2/pokemon/charizard";
 
     const [data, setData] = useState();
     const [name, setName] = useState();
 
-    useEffect(() => {
+    const apiURL = `https://pokeapi.co/api/v2/pokemon/${data}`;
+
+
+
+
+    const searchPokemon = () => {
         axios.get(apiURL).then((response) => {
          
             setData(response.data);
             setName(response.data.name)
         }).catch((err) => {
-            windows.alert(err);
+            console.log(err);
         })
+    }
+
+    useEffect(() => {
+     
     })
 
      return(
         <div>
             <h2>{name}</h2>
             <input type="text" placeholder='type here..'/>
-            <input type="submit" />
-            <img src={data.sprites.other.dream_world.front_default} alt='pokemon' />
+            <button onClick={searchPokemon}>Search For The Pokemons</button>
+            {/* <img src={data.sprites.other.dream_world.front_default} alt='pokemon' /> */}
             
         </div>
      );
